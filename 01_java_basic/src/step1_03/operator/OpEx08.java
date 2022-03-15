131262ef953346ece8a81a5421890aa0b58cc2f3
piggyrabbit0308 <piggyrabbit0308@naver.com>
2022-03-15 16:10:49 +0900

    Revert "2022-03-15 복습"
    This reverts commit bb726c6db7679973e756fac83b3d60c96d6dbf86.

--- 01_java_basic/src/step1_03/operator/OpEx08.java
+++ 01_java_basic/src/step1_03/operator/OpEx08.java
@@ -4,9 +4,9 @@ public class OpEx08 {
 	public static void main(String[] args) {
 		int test = 60;
 		
-		System.out.println(test >= 60 ? 10 : 100);
+		System.out.println(test >= 60 ? "합격" : "불합격");
 		
-		int result = test >= 60 ? 1 : 2;
+		char result = test >= 60 ? 'p' : 'f';
 		System.out.println(result);
 	}
 }
